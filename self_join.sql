94557d5b5f8a4cdd7c682b13f7a7775135c94c96
Sergii Golovko <sergii.golovko@gmail.com>
2016-09-17 21:55:47 -0400

    Added solutions to problems 7-9

--- self_join.sql
+++ self_join.sql
@@ -48,7 +48,36 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart'
-AND stopb.name='London Road'				 
+AND stopb.name='London Road'
+
+# 7. Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON 
+	(a.company = b.company AND a.num = b.num)
+WHERE (a.stop = 115) AND (b.stop = 137) 
 
 # 8. Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
-SELECT 
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON 
+	(a.company = b.company AND a.num = b.num)
+WHERE a.stop IN (SELECT id
+  				 FROM stops s
+			     WHERE s.name='Craiglockhart')
+AND b.stop IN (SELECT id
+               FROM stops s
+		       WHERE s.name='Tollcross') 
+
+# 9. Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
+
+SELECT DISTINCT s.name, a.company, a.num
+FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+             JOIN stops s ON (s.id = b.stop)
+WHERE a.stop IN (SELECT id
+                 FROM stops
+			     WHERE name = 'Craiglockhart')
+AND a.company='LRT' 
+
+# 10. Find the routes involving two buses that can go from Craiglockhart to Sighthill. Show the bus no. and company for the first bus, the name of the stop for the transfer, and the bus no. and company for the second bus.
+
